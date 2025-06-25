2014c5900345366ce3db7515ef42807d6c675597
Pedro <pedrosloiola@gmail.com>
2025-06-24 23:30:29 -0300

    Update StatusCliente.java

--- StatusCliente.java
+++ StatusCliente.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
+// TESTES USANDO GET
 public class StatusCliente {
     public static void main(String[] args) {
         try {
