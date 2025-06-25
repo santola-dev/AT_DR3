858acb554258dc14ad1a7d3193ecd8209d720e3b
Pedro <pedrosloiola@gmail.com>
2025-06-24 23:30:02 -0300

    Update GetTodosItensCliente.java

--- GetTodosItensCliente.java
+++ GetTodosItensCliente.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+// TESTES USANDO GET
 public class GetTodosItensCliente {
     public static void main(String[] args) {
         try {
